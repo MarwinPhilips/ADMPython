9ee6cd780eecc37d7d2df6451a96825770ac5e81
BurningBadger <hosmann@gmx.net>
2017-06-11 16:24:24 +0200

    efficiency update & code cleanup in task1

--- MapReduce/multiprocessing_Task1.py
+++ MapReduce/multiprocessing_Task1.py
@@ -1,4 +1,3 @@
-import csv
 import datetime
 import multiprocessing
 import collections
@@ -14,25 +13,30 @@ def task1map(file):
     start_time = datetime.datetime.now()
     print(multiprocessing.current_process().name + " started mapping.")
     with open(file) as f:
-        data = csv.DictReader(f, dialect='excel')
+        data = pandas.read_csv(f, dialect='excel', index_col=0)
         timestamps = []
-        for row in data:
-            if row['code']=='res_snd':
-                timestamps.append(int(row['time'])//60000)
-    stop_time = datetime.datetime.now()
-    running_time = stop_time - start_time
-    print(multiprocessing.current_process().name + " finished mapping. Time spent: " + str(running_time))
+        for row in zip(data['code'], data['time']):
+            if row[0]=='res_snd':
+                timestamps.append(int(row[1])//60000)
+    print(multiprocessing.current_process().name + " finished mapping. Time spent: " + str(datetime.datetime.now() - start_time))
     return timestamps
 
 
 def task1shuffle(data):
-    l = (sorted(list(data)),)
-    return l
+    start_time = datetime.datetime.now()
+    print("Everyday I'm shuffeling...")
+    mergedlist =[]
+    for list in data:
+        mergedlist += list
+    l = sorted(mergedlist)
+    count = [dict(collections.Counter(l))]
+    print("Finished shuffeling. Time spent: " + str(datetime.datetime.now() - start_time))
+    return count
 
 
-def task1reduce(timestamps):
-    print(multiprocessing.current_process().name + " started reducing.")
-    return collections.Counter(timestamps)
+def task1reduce(counts):
+    # since it makes no sense to use multiprocessing in this reduce task, all reducing is done in the shuffle-function
+    return counts
 
 if __name__ == '__main__':
     import glob
